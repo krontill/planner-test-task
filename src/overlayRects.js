bc0cdc2c18b8e9878d36285c66cbc2a2c09b92e9
s.shcherba <s.shcherba@itransition.com>
2018-12-22 13:09:55 +0300

    Change overlayRects.

--- src/overlayRects.js
+++ src/overlayRects.js
@@ -1,18 +1,32 @@
 import rectsArray from './rectsArray';
-import {defaultColorRect, overlayColorRect} from './rectColors';
 
 const overlayRects = (moveRectIndex) => {
-    rectsArray.forEach((rect) => {
-        if ((rectsArray[moveRectIndex].x > rect.x && rectsArray[moveRectIndex].x < rect.x + rect.width
-            || rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width > rect.x && rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width < rect.x + rect.width)
-            && (rectsArray[moveRectIndex].y > rect.y && rectsArray[moveRectIndex].y < rect.y + rect.height
-                || rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height > rect.y && rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height < rect.y + rect.height)) {
-            rectsArray[moveRectIndex].fill = overlayColorRect;
-            rect.fill = overlayColorRect;
-        } else {
-            rect.fill = defaultColorRect;
+    let overlayRects = [];
+    rectsArray.forEach((rect, index) => {
+        const overlayLeftSideRect = rectsArray[moveRectIndex].x >= rect.x
+            && rectsArray[moveRectIndex].x <= rect.x + rect.width;
+        const overlayRightSideRect = rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width >= rect.x
+            && rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width <= rect.x + rect.width;
+        const overlayAxisX = rectsArray[moveRectIndex].x <= rect.x
+            && rectsArray[moveRectIndex].x <= rect.x + rect.width
+            && rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width >= rect.x
+            && rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width >= rect.x + rect.width;
+
+        const overlayTopSideRect = rectsArray[moveRectIndex].y >= rect.y
+            && rectsArray[moveRectIndex].y <= rect.y + rect.height;
+        const overlayBottomSideRect = rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height >= rect.y
+            && rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height <= rect.y + rect.height;
+        const overlayAxisY = rectsArray[moveRectIndex].y <= rect.y
+            && rectsArray[moveRectIndex].y <= rect.y + rect.height
+            && rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height >= rect.y
+            && rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height >= rect.y + rect.height;
+
+        if ((overlayLeftSideRect || overlayRightSideRect || overlayAxisX)
+            && (overlayTopSideRect || overlayBottomSideRect || overlayAxisY)) {
+            overlayRects.push(index);
         }
     });
+    return overlayRects;
 };
 
 export default overlayRects;
