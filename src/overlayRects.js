f62f521e326dac88ed70aa261967371b45b4baea
s.shcherba <s.shcherba@itransition.com>
2018-12-26 14:54:25 +0300

    Add test for overlayRects function.

--- src/overlayRects.js
+++ src/overlayRects.js
@@ -1,32 +1,34 @@
 import rectsArray from './rectsArray';
 
-const overlayRects = (moveRectIndex, distance = 0) => {
+const overlayRects = (moveRectIndex = null, distance = 0) => {
     let overlayRects = [];
-    rectsArray.forEach((rect, index) => {
-        const overlayLeftSideRect = rectsArray[moveRectIndex].x - distance > rect.x
-            && rectsArray[moveRectIndex].x - distance < rect.x + rect.width;
-        const overlayRightSideRect = rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width + distance > rect.x
-            && rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width + distance < rect.x + rect.width;
-        const overlayAxisX = rectsArray[moveRectIndex].x - distance < rect.x
-            && rectsArray[moveRectIndex].x - distance < rect.x + rect.width
-            && rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width + distance > rect.x
-            && rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width + distance > rect.x + rect.width;
+    if (moveRectIndex !== null) {
+        rectsArray.forEach((rect, index) => {
+            const overlayLeftSideRect = rectsArray[moveRectIndex].x - distance > rect.x
+                && rectsArray[moveRectIndex].x - distance < rect.x + rect.width;
+            const overlayRightSideRect = rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width + distance > rect.x
+                && rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width + distance < rect.x + rect.width;
+            const overlayAxisX = rectsArray[moveRectIndex].x - distance < rect.x
+                && rectsArray[moveRectIndex].x - distance < rect.x + rect.width
+                && rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width + distance > rect.x
+                && rectsArray[moveRectIndex].x + rectsArray[moveRectIndex].width + distance > rect.x + rect.width;
 
-        const overlayTopSideRect = rectsArray[moveRectIndex].y - distance > rect.y
-            && rectsArray[moveRectIndex].y - distance < rect.y + rect.height;
-        const overlayBottomSideRect = rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height + distance > rect.y
-            && rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height + distance < rect.y + rect.height;
-        const overlayAxisY = rectsArray[moveRectIndex].y - distance < rect.y
-            && rectsArray[moveRectIndex].y - distance < rect.y + rect.height
-            && rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height + distance > rect.y
-            && rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height + distance > rect.y + rect.height;
+            const overlayTopSideRect = rectsArray[moveRectIndex].y - distance > rect.y
+                && rectsArray[moveRectIndex].y - distance < rect.y + rect.height;
+            const overlayBottomSideRect = rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height + distance > rect.y
+                && rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height + distance < rect.y + rect.height;
+            const overlayAxisY = rectsArray[moveRectIndex].y - distance < rect.y
+                && rectsArray[moveRectIndex].y - distance < rect.y + rect.height
+                && rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height + distance > rect.y
+                && rectsArray[moveRectIndex].y + rectsArray[moveRectIndex].height + distance > rect.y + rect.height;
 
-        if ((overlayLeftSideRect || overlayRightSideRect || overlayAxisX)
-            && (overlayTopSideRect || overlayBottomSideRect || overlayAxisY)) {
-            overlayRects.push(index);
-        }
-    });
-    overlayRects.push(moveRectIndex);
+            if ((overlayLeftSideRect || overlayRightSideRect || overlayAxisX)
+                && (overlayTopSideRect || overlayBottomSideRect || overlayAxisY)) {
+                overlayRects.push(index);
+            }
+        });
+        overlayRects.push(moveRectIndex);
+    }
     return overlayRects;
 };
 
