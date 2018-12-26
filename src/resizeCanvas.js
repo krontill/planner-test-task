708c24f3b2cbb7183612033507ef23e22f1b1726
s.shcherba <s.shcherba@itransition.com>
2018-12-26 16:46:06 +0300

    Remove old comments.

--- src/resizeCanvas.js
+++ src/resizeCanvas.js
@@ -2,8 +2,8 @@ import drawingRects from "./drawingRects";
 
 const padding = 30;
 const resizeCanvas = (canvas, ctx) => {
-    const width = window.innerWidth - padding; //document.documentElement.clientWidth - если учитывать скролл
-    const height = window.innerHeight - padding; //document.documentElement.clientHeight
+    const width = window.innerWidth - padding;
+    const height = window.innerHeight - padding;
     if (width !== canvas.width) {
         canvas.width = width;
     }
